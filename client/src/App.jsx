501fbcdf62612a46e8b71bae6d9b5c577cce30e5
itxSaaad <saadstudent.cs@gmail.com>
2024-11-06 21:30:02 +0500

    Added: Coming Soon Page and Custom Animations

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,18 @@
 import { Helmet } from 'react-helmet-async';
+
+import ComingSoon from './components/ui/ComingSoon';
+
 function App() {
   return (
     <>
       <Helmet>
-        <title>OptaHire - Revolutionizing Talent Acquisition</title>
+        <title>Coming Soon - OptaHire</title>
         <meta
           name="description"
-          content="OptaHire is a recruitment platform that helps you find the best job for you."
+          content="OptaHire is working hard to bring you an innovative recruitment platform. Stay tuned for something amazing!"
         />
       </Helmet>
-      <section className="bg-gray-100 text-gray-800 min-h-screen flex items-center justify-center">
-        <div className="max-w-md w-full p-4 bg-white rounded-lg shadow-lg">
-          <h1 className="text-2xl font-semibold mb-4">Hello, Tailwind CSS!</h1>
-          <p className="text-gray-600">
-            This is a simple Tailwind CSS template.
-          </p>
-        </div>
-      </section>
+      <ComingSoon />
     </>
   );
 }
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,46 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#0EB0E3',
+        secondary: '#3946AE',
+        darkBackground: '#121212',
+        darkText: '#E0E0E0',
+      },
+      animation: {
+        loader:
+          'fadeIn 0.3s ease-in, spin 3s linear infinite,scale 0.3s ease-in',
+      },
+      keyframes: {
+        spin: {
+          '0%': { transform: 'rotate(0deg)' },
+          '100%': { transform: 'rotate(360deg)' },
+        },
+        scale: {
+          '0%': { transform: 'scale(0)' },
+          '100%': { transform: 'scale(1)' },
+        },
+        fadeIn: {
+          '0%': { opacity: 0 },
+          '100%': { opacity: 1 },
+        },
+        fadeOut: {
+          '0%': { opacity: 1 },
+          '100%': { opacity: 0 },
+        },
+        slideIn: {
+          '0%': { transform: 'translateX(100%)' },
+          '100%': { transform: 'translateX(0)' },
+        },
+        slideOut: {
+          '0%': { transform: 'translateX(0)' },
+          '100%': { transform: 'translateX(100%)' },
+        },
+      },
+    },
   },
   plugins: [],
 };
