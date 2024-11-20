25914d2f6917652a22bcee86ddc8d67f4fe39d3f
itxSaaad <saadstudent.cs@gmail.com>
2024-11-20 22:53:45 +0500

    Added: Coming Soon Page to Every other Route

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -13,17 +13,15 @@ import './index.css';
 import App from './App.jsx';
 import ComingSoon from './screens/ComingSoon.jsx';
 import HomeScreen from './screens/HomeScreen.jsx';
-import LoginScreen from './screens/LoginScreen.jsx';
 import NotFoundScreen from './screens/NotFoundScreen.jsx';
-import RegisterScreen from './screens/RegisterScreen.jsx';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route element={<App />}>
       <Route index element={<HomeScreen />} />
       <Route path="coming-soon" element={<ComingSoon />} />
-      <Route path="login" element={<LoginScreen />} />
-      <Route path="register" element={<RegisterScreen />} />
+      <Route path="login" element={<ComingSoon />} />
+      <Route path="register" element={<ComingSoon />} />
       <Route path="jobs" element={<ComingSoon />} />
       <Route path="recruiter/*" element={<ComingSoon />} />
       <Route path="interviewer/*" element={<ComingSoon />} />
