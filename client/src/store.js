1f76b9723febf74dea00f60e479d3bb7fd037144
Muhammad Saad <saadstudent.cs@gmail.com>
2025-02-27 21:33:15 +0500

    Added: authentication API and slice; integrated Redux store with auth management

--- client/src/store.js
+++ client/src/store.js
@@ -1,8 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
 
+import { authApi } from './features/auth/authApi';
+import authReducer from './features/auth/authSlice';
+
 const store = configureStore({
-  reducer: {},
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
+  reducer: {
+    auth: authReducer,
+    [authApi.reducerPath]: authApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(authApi.middleware),
 });
 
 export default store;
